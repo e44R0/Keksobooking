1285cdc13fca6e8f91c420a1958928f46af9f5da
corpp <corppwork@gmail.com>
2024-05-01 19:18:37 +0300

    модуль 4 задача 1. Тестовые данные

--- js/main.js
+++ js/main.js
@@ -1,20 +1,65 @@
+const AD_QUANTITY = 10;
+const CHECK_TIME = ['12:00', '13:00', '14:00'];
+const APARTMENT_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const APARTMENT_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const TITLE = [
+  'Посуточная аренда',
+  'Квартира в аренду',
+  'Продам квартиру',
+  'Продам дом'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.'];
+const DESCRIPTION = [
+  'Уютная студия у метро. Wi-Fi.',
+  'Центр. Просторная квартира. Парковка.',
+  'Панорамный вид. Балкон. Тихий район.'];
+
+let getRandomArrayElement = (array) => array[getRandInt(0, array.length - 1)];
+
 let getRandInt = (min, max) => {
   if (min < 0 || max < 0) {
     return NaN
   }
-  min > max ? [min,max] = [max, min] : [min,max] = [min,max] ;  //Деструктурирование.. можно ли так или это несовсем корректно?
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
+  // min > max ? [min,max] = [max, min] : [min,max] = [min,max] ;  //Деструктурирование.. можно ли так или это несовсем корректно?
+  const minCeiled = Math.ceil(Math.min(min,max));
+  const maxFloored = Math.floor(Math.max(min,max));
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-let getRandFloat = (min, max, accuracy) => {
-  if (min < 0 || max < 0) {
+let getRandFloat = (min, max, accuracy = 1) => {
+  if (min < 0 || max < 0 || accuracy < 0 ) {
     return NaN
   }
-  min > max ? [min,max] = [max, min] : [min,max] = [min,max] ;
-  return (Math.random() * (max - min) + min).toFixed(accuracy);
+  const lower = Math.min(min,max);
+  const upper = Math.max(min,max);
+  return (Math.random() * (upper - lower) + lower).toFixed(accuracy);
 }
 
-console.log('Ранодомное целое число: ' + getRandInt(10,1));
-console.log('Ранодомное дробное число: ' + getRandFloat(1.1,1.2,4));
+const createAdData = (quantity) => ({
+  author :  {
+    avatar : `img/avatars/user${ quantity < 10 ? '0'.concat(quantity) : quantity }.png`
+  },
+  location : {
+    lat : getRandFloat(35.65000,35.70000,5),
+    lng : getRandFloat(139.70000,139.80000,5)
+  },
+  order : {
+    title : getRandomArrayElement(TITLE),
+    address : `${location.lat},${location.lng}`, //не работает =(
+    price : getRandInt(1500,5000),
+    type : getRandomArrayElement(APARTMENT_TYPE),
+    rooms : getRandInt(1,5),
+    guests : getRandInt(1,10),
+    checkin : getRandomArrayElement(CHECK_TIME),
+    checkout : getRandomArrayElement(CHECK_TIME),
+    features : Array.from({length: getRandInt(1,APARTMENT_FEATURES.length - 1)}, () => getRandomArrayElement(APARTMENT_FEATURES)) ,
+    description : getRandomArrayElement(DESCRIPTION),
+    photos : getRandomArrayElement(PHOTOS),
+  }
+});
+
+const generateData = Array.from({length: AD_QUANTITY}, (element,i) => createAdData( i + 1 ));
+
+console.log(generateData);
